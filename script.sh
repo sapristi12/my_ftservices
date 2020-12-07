7f6d8be1f077a02e4c3741d311628c73482d6474
erwan ljq <er.lajouannique@gmail.com>
2020-12-07 11:26:03 +0100

    push

---
+++ script.sh
@@ -0,0 +1,28 @@
+minikube start
+
+minikube ip
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl apply -f metalLB.yaml
+kubectl create secret generic -n metallb-system memberlist  --from-literal=secretkey="$(openssl rand -base64 128)"
+
+eval $(minikube docker-env)
+IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
+printf "Minikube IP: ${IP}"
+
+#DOCKERS
+echo "Dockers :"
+docker build -t service_nginx ./nginx
+#docker build -t service_ftps --build-arg IP=${IP} ./ftps
+#docker build -t service_phpmyadmin --build-arg IP=${IP} ./phpmyadmin
+#docker build -t service_influxdb --build-arg IP=${IP} ./influxdb
+
+#YAML
+echo "Pods and services :"
+kubectl create -f ./nginx.yaml
+#kubectl create -f ./ftps.yaml
+#kubectl create -f ./phpmyadmin.yaml
+#kubectl create -f ./influxdb.yaml
+
+minikube dashboard
