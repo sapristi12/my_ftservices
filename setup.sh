9cf7f890be3912e4bc5d9f40e8371a3889dc9732
Erwan Lajouannique <er.lajouannique@gmail.com>
2020-12-23 10:57:56 +0100

    wordpress database

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ echo "\e[32mOK !"
 echo "\e[34mInstallation de metalLB..."
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl apply -f metalLB.yaml
+kubectl apply -f ./srcs/metalLB.yaml
 kubectl create secret generic -n metallb-system memberlist  --from-literal=secretkey="$(openssl rand -base64 128)"
 echo "\e[32mOK !"
 
@@ -19,33 +19,33 @@ eval $(minikube docker-env)
 
 echo "\e[34mCréations des dockers..."
 
-echo "\e[93mBuilding image nginx"
-#docker build -t nginx ./nginx > /dev/null
-echo "\e[93mBuilding image ftps"
-docker build -t ftps ./ftps > /dev/null
+#echo "\e[93mBuilding image nginx"
+#docker build -t nginx ./srcs/nginx > /dev/null
+#echo "\e[93mBuilding image ftps"
+#docker build -t ftps ./srcs/ftps > /dev/null
 echo "\e[93mBuilding image mysql"
-#docker build -t mysql ./mysql > /dev/null
+docker build -t mysql ./srcs/mysql > /dev/null
 echo "\e[93mBuilding image wordpress"
-#docker build -t wordpress ./wordpress > /dev/null
+docker build -t wordpress ./srcs/wordpress > /dev/null
 echo "\e[93mBuilding image phpmyadmin"
-#docker build -t phpmyadmin ./phpmyadmin > /dev/null
-echo "\e[93mBuilding image influxdb"
-#docker build -t influxdb ./influxdb > /dev/null
-echo "\e[93mBuilding image telegraf"
-#docker build -t telegraf ./telegraf > /dev/null
-echo "\e[93mBuilding image grafana"
-#docker build -t grafana ./grafana > /dev/null
-echo "\e[32mOK !"
-
-echo "\e[34mApplication des fichiers .yaml..."
-#kubectl create -f ./nginx.yaml > /dev/null
-#kubectl create -f ./mysql.yaml > /dev/null
-#kubectl create -f ./wordpress.yaml > /dev/null
-kubectl create -f ./ftps.yaml > /dev/null
-#kubectl create -f ./phpmyadmin.yaml > /dev/null
-#kubectl create -f ./influxdb.yaml > /dev/null
-#kubectl create -f ./telegraf.yaml > /dev/null
-#kubectl create -f ./grafana.yaml > /dev/null
+docker build -t phpmyadmin ./srcs/phpmyadmin > /dev/null
+#echo "\e[93mBuilding image influxdb"
+#docker build -t influxdb ./srcs/influxdb > /dev/null
+#echo "\e[93mBuilding image telegraf"
+#docker build -t telegraf ./srcs/telegraf > /dev/null
+#echo "\e[93mBuilding image grafana"
+#docker build -t grafana ./srcs/grafana > /dev/null
+#echo "\e[32mOK !"
+#
+#echo "\e[34mApplication des fichiers .yaml..."
+#kubectl create -f ./srcs/nginx.yaml > /dev/null
+#kubectl create -f ./srcs/mysql.yaml > /dev/null
+#kubectl create -f ./srcs/wordpress.yaml > /dev/null
+#kubectl create -f ./srcs/ftps.yaml > /dev/null
+#kubectl create -f ./srcs/phpmyadmin.yaml > /dev/null
+#kubectl create -f ./srcs/influxdb.yaml > /dev/null
+#kubectl create -f ./srcs/telegraf.yaml > /dev/null
+#kubectl create -f ./srcs/grafana.yaml > /dev/null
 echo "\e[32mOK !"
 echo "=== PHPMYADMIN ==="
 echo "Login : erlajoua | password : 1212"
